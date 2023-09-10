90429c96e80a63e8755587fed402709c82395de3
Holly <hollyxue05@gmail.com>
2023-09-09 20:15:25 -0400

    Add EditText and name TextView

--- app/src/main/java/com/example/l02_guiinterations/MainActivity.java
+++ app/src/main/java/com/example/l02_guiinterations/MainActivity.java
@@ -5,25 +5,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button incrementButton;
+    Button incrementButton, submitButton;
     TextView greetingDisplay;
+    TextView nameDisplay;
     int count = 0;
     String[] planets;
     RadioButton leftRadBut, rightRadBut;
+    EditText nameInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         incrementButton = findViewById(R.id.increment_button);
+        submitButton = findViewById(R.id.submit_button);
         greetingDisplay = findViewById(R.id.greeting_textview);
+        nameDisplay = findViewById(R.id.whatName_textview);
         planets = getResources().getStringArray(R.array.planets_array);
         leftRadBut = findViewById(R.id.radio_left);
         rightRadBut = findViewById(R.id.radio_right);
+        nameInput = findViewById(R.id.editTextName);
         incrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
                 if(rightRadBut.isChecked())
                     count = ++count % planets.length;
                 greetingDisplay.setText(planets[count]);
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameDisplay.setText("Hi " + nameInput.getText() + "!");
             }
         });
     }
